e7674792b66cd3e947faac94742071e3a4b62f76
AboubacarSiriki <sidiksoum344@gmail.com>
2024-01-14 11:15:53 +0000

    Sanogo

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.secret_key = "votre_clé_secrète"
 # Configuration de la connexion à SQL Server
 app.config["SQL_SERVER_CONNECTION_STRING"] = """
     Driver={SQL Server};
-    Server=DESKTOP-6RB7ER5\SQLEXPRESS;
+    Server=DESKTOP-5I6GQ70\SQLEXPRESS;
     Database=MV;
     Trusted_Connection=yes;"""
 
