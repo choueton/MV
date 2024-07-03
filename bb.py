4770d4db6a243f00b867673d7ddba9e6394f8eba
choueton <sanogoprince565@gmail.com>
2024-07-03 21:55:00 +0000

    deux test model

--- bb.py
+++ bb.py
@@ -1,262 +1,73 @@
-# Route pour gérer les actions de like/dislike
-# @app.route('/like/<int:IdLocations>', methods=["GET", "POST"])
-# def like_location(IdLocations):
-#     # data = request.json
-#     location_id = IdLocations
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
 
-#     # Récupérer l'ID de l'utilisateur
-#     user_id = get_user_id()
+st.write("L'application qui prédit l'accord du crédit")
 
-#     # Connexion à la base de données
-#     conn = pyodbc.connect(app.config["SQL_SERVER_CONNECTION_STRING"])
-#     cursor = conn.cursor()
+#Collecter le profil d'entrée
+st.sidebar.header("Les caracteristiques du client")
 
-#     # Vérification si l'utilisateur a déjà aimé ou non cet emplacement
-#     cursor.execute("SELECT type_action FROM like_location WHERE idutilisateur = ? AND IdLocations = ?", (user_id, location_id))
-#     previous_action = cursor.fetchone()
+def client_caract_entree():
+    Gender=st.sidebar.selectbox('Sexe',('Male','Female'))
+    Married=st.sidebar.selectbox('Marié',('Yes','No'))
+    Dependents=st.sidebar.selectbox('Enfants',('0','1','2','3+'))
+    Education=st.sidebar.selectbox('Education',('Graduate','Not Graduate'))
+    Self_Employed=st.sidebar.selectbox('Salarié ou Entrepreneur',('Yes','No'))
+    ApplicantIncome=st.sidebar.slider('Salaire du client',150,4000,200)
+    CoapplicantIncome=st.sidebar.slider('Salaire du conjoint',0,40000,2000)
+    LoanAmount=st.sidebar.slider('Montant du crédit en Kdollar',9.0,700.0,200.0)
+    Loan_Amount_Term=st.sidebar.selectbox('Durée du crédit',(360.0,120.0,240.0,180.0,60.0,300.0,36.0,84.0,12.0))
+    Credit_History=st.sidebar.selectbox('Credit_History',(1.0,0.0))
+    Property_Area=st.sidebar.selectbox('Property_Area',('Urban','Rural','Semiurban'))
 
-#     if previous_action:
-#         if previous_action[0] == 'like':
-#             action = 'dislike'
-#             cursor.execute("UPDATE like_location SET type_action = ? WHERE idutilisateur = ? AND IdLocations = ?", (action, user_id, location_id))
-#             # Réduire le nombre de likes dans la table Locations
-#             cursor.execute("UPDATE Locations SET Likes = Likes - 1 WHERE IdLocations = ?", (location_id,))
-#         else:
-#             action = 'like'
-#             cursor.execute("UPDATE like_location SET type_action = ? WHERE idutilisateur = ? AND IdLocations = ?", (action, user_id, location_id))
-#             # Augmenter le nombre de likes dans la table Locations
-#             cursor.execute("UPDATE Locations SET Likes = Likes + 1 WHERE IdLocations = ?", (location_id,))
-#     else:
-#         # Ajoute une nouvelle entrée pour l'utilisateur
-#         action = 'like'
-#         cursor.execute("INSERT INTO like_location (idutilisateur, IdLocations, type_action) VALUES (?, ?, ?)", (user_id, location_id, action))
-#         # Augmenter le nombre de likes dans la table Locations
-#         cursor.execute("UPDATE Locations SET Likes = Likes + 1 WHERE IdLocations = ?", (location_id,))
+    data={
+    'Gender':Gender,
+    'Married':Married,
+    'Dependents':Dependents,
+    'Education':Education,
+    'Self_Employed':Self_Employed,
+    'ApplicantIncome':ApplicantIncome,
+    'CoapplicantIncome':CoapplicantIncome,
+    'LoanAmount':LoanAmount,
+    'Loan_Amount_Term':Loan_Amount_Term,
+    'Credit_History':Credit_History,
+    'Property_Area':Property_Area
+    }
 
-#     # Commit et fermeture de la connexion à la base de données
-#     conn.commit()
-#     conn.close()
-    
-#     # Si l'utilisateur est connecté, mettre à jour l'identifiant dans la base de données
-#     if 'IdUtilisateur' in session:
-#         cursor = conn.cursor()
-#         cursor.execute("UPDATE like_location SET idutilisateur = ? WHERE idutilisateur = ?", (session['IdUtilisateur'], user_id))
-#         conn.commit()
-#         conn.close()
-#         # Supprimer le cookie car l'utilisateur est connecté
-#         session.pop("Idcooki", None)
+    profil_client=pd.DataFrame(data,index=[0])
+    return profil_client
 
-#     return redirect(url_for("loue_maison"))
+input_df=client_caract_entree()
 
 
 
+#Transformer les données d'entrée en données adaptées à notre modèle
+#importer la base de données
+df=pd.read_csv('train.csv')
+credit_input=df.drop(columns=['Loan_ID','Loan_Status'])
+donnee_entree=pd.concat([input_df,credit_input],axis=0)
+
+# encodage des données
+var_cat=['Gender', 'Married', 'Dependents', 'Education','Self_Employed','Credit_History', 'Property_Area']
+for col in var_cat:
+    dummy=pd.get_dummies(donnee_entree[col],drop_first=True)
+    donnee_entree=pd.concat([dummy,donnee_entree],axis=1)
+    del donnee_entree[col]
+#prendre uniquement la premiere ligne
+donnee_entree=donnee_entree[:1]
+
+#afficher les données transformées
+st.subheader('Les caracteristiques transformés')
+st.write(donnee_entree)
 
 
-# @app.route("/loue_maison", methods=["GET", "POST"])
-# def loue_maison():
-#     # Connexion à la base de données
-#     conn = pyodbc.connect(app.config["SQL_SERVER_CONNECTION_STRING"])
-#     cursor = conn.cursor()
-
-#     # Récupération de la page actuelle depuis l'URL
-#     page = request.args.get(get_page_parameter(), type=int, default=1)
-#     per_page = 5  # Nombre d'éléments à afficher par page
-
-#     # Récupérer les filtres
-#     ville = request.args.get("Ville")
-#     commune = request.args.get("Commune")
-#     nombre_de_pieces = request.args.get("Nombre_de_pieces")
-#     prix_min = request.args.get("Prix_min")
-#     prix_max = request.args.get("Prix_max")
-
-#     # Construction de la requête SQL en fonction des filtres
-#     query_count = "SELECT COUNT(*) FROM Locations WHERE 1=1"
-#     params_count = []
-#     if ville:
-#         query_count += " AND Ville = ?"
-#         params_count.append(ville)
-#     if commune:
-#         query_count += " AND Commune = ?"
-#         params_count.append(commune)
-#     if nombre_de_pieces:
-#         query_count += " AND Nombre_de_pieces = ?"
-#         params_count.append(nombre_de_pieces)
-#     if prix_min:
-#         query_count += " AND Prix_mensuel >= ?"
-#         params_count.append(prix_min)
-#     if prix_max:
-#         query_count += " AND Prix_mensuel <= ?"
-#         params_count.append(prix_max)
-
-#     # Exécution de la requête pour obtenir le nombre total d'éléments
-#     cursor.execute(query_count, params_count)
-#     total_count = cursor.fetchone()[0]
-
-#     # Calcul de l'offset à partir de la page et du nombre d'éléments par page
-#     offset = (page - 1) * per_page
-
-#     # Construction de la requête SQL pour récupérer les éléments à afficher
-#     query_select = """
-#     SELECT l.*, COALESCE(ul.type_action, 'none') as type_action
-#     FROM Locations l
-#     LEFT JOIN user_like ul ON l.IdLocations = ul.IdLocations AND ul.idutilisateur = ?
-#     WHERE 1=1
-#     """
-#     params_select = [get_user_id()]
-#     if ville:
-#         query_select += " AND Ville = ?"
-#         params_select.append(ville)
-#     if commune:
-#         query_select += " AND Commune = ?"
-#         params_select.append(commune)
-#     if nombre_de_pieces:
-#         query_select += " AND Nombre_de_pieces = ?"
-#         params_select.append(nombre_de_pieces)
-#     if prix_min:
-#         query_select += " AND Prix_mensuel >= ?"
-#         params_select.append(prix_min)
-#     if prix_max:
-#         query_select += " AND Prix_mensuel <= ?"
-#         params_select.append(prix_max)
-    
-#     query_select += " ORDER BY IdLocations OFFSET ? ROWS FETCH NEXT ? ROWS ONLY"
-#     params_select += [offset, per_page]
-
-#     # Exécution de la requête pour obtenir les éléments à afficher
-#     cursor.execute(query_select, params_select)
-#     loc_afi = cursor.fetchall()
-
-#     # Initialize an empty list to store processed data
-#     locations = []
-
-#     for row in loc_afi:
-#         # Process each row and extract the Image1 attribute
-#         images = row.Image1.split(",") if row.Image1 else []
-#         # Append a dictionary with the processed data to the locations list
-#         locations.append({"row": row, "images": images, "liked": row.type_action == 'like'})
-
-#     # Configuration de la pagination avec le nombre total d'éléments et le nombre d'éléments par page
-#     pagination = Pagination(page=page, total=total_count, per_page=per_page, css_framework="bootstrap5")
-
-#     # Fermeture du curseur et de la connexion
-#     cursor.close()
-#     conn.close()
-
-#     # Rendu du template avec les données récupérées et la pagination
-#     return render_template("/page/loue_maison.html", locations=locations, pagination=pagination)
+#importer le modèle
+load_model=pickle.load(open('prevision_credit.pkl','rb'))
 
 
+#appliquer le modèle sur le profil d'entrée
+prevision=load_model.predict(donnee_entree)
 
-# @app.route("/achete_maison", methods=["GET", "POST"])
-# def achete_maison():
-
-#     # Connexion à la base de données
-#     conn = pyodbc.connect(app.config["SQL_SERVER_CONNECTION_STRING"])
-#     cursor = conn.cursor()
-
-#     # Récupération de la page actuelle depuis l'URL
-#     page = request.args.get(get_page_parameter(), type=int, default=1)
-
-#     # Nombre d'éléments à afficher par page
-#     per_page = 5
-
-#     # Définition des filtres
-#     ville = request.args.get("Ville")
-#     commune = request.args.get("Commune")
-#     nombre_de_pieces = request.args.get("Nombre_de_pieces")
-#     prix_min = request.args.get("Prix_min")
-#     prix_max = request.args.get("Prix_max")
-
-#     # Construction de la requête SQL en fonction des filtres
-#     query_count = "SELECT COUNT(*) FROM Maison WHERE 1=1"
-#     params_count = []
-
-#     if ville:
-#         query_count += " AND Ville = ?"
-#         params_count.append(ville)
-#     if commune:
-#         query_count += " AND Commune = ?"
-#         params_count.append(commune)
-#     if nombre_de_pieces:
-#         query_count += " AND Nombre_de_pieces = ?"
-#         params_count.append(nombre_de_pieces)
-#     if prix_min:
-#         query_count += " AND Prix_unitaire >= ?"
-#         params_count.append(prix_min)
-#     if prix_max:
-#         query_count += " AND Prix_unitaire <= ?"
-#         params_count.append(prix_max)
-
-#     # Exécution de la requête pour obtenir le nombre total d'éléments
-#     cursor.execute(query_count, params_count)
-#     total_count = cursor.fetchone()[0]
-
-#     # Calcul de l'offset à partir de la page et du nombre d'éléments par page
-#     offset = (page - 1) * per_page
-
-#     # Construction de la requête SQL pour récupérer les éléments à afficher
-#     query_select = """
-#     SELECT m.*, COALESCE(ul.type_action, 'none') as type_action
-#     FROM Maison m
-#     LEFT JOIN user_like ul ON m.IdMaison = ul.IdMaison AND ul.idutilisateur = ?
-#     WHERE 1=1
-#     """
-#     params_select = [get_user_id()]
-
-#     if ville:
-#         query_select += " AND Ville = ?"
-#         params_select.append(ville)
-#     if commune:
-#         query_select += " AND Commune = ?"
-#         params_select.append(commune)
-#     if nombre_de_pieces:
-#         query_select += " AND Nombre_de_pieces = ?"
-#         params_select.append(nombre_de_pieces)
-#     if prix_min:
-#         query_select += " AND Prix_unitaire >= ?"
-#         params_select.append(prix_min)
-#     if prix_max:
-#         query_select += " AND Prix_unitaire <= ?"
-#         params_select.append(prix_max)
-
-#     query_select += " ORDER BY IdMaison OFFSET ? ROWS FETCH NEXT ? ROWS ONLY"
-#     params_select += [offset, per_page]
-
-#     # Exécution de la requête pour obtenir les éléments à afficher
-#     cursor.execute(query_select, params_select)
-#     maison_ifo = cursor.fetchall()
-
-#     maison = []
-
-#     for row in maison_ifo:
-#         # Process each row and extract the Image1 attribute
-#         images = row.Image1.split(",") if row.Image1 else []
-#         # Append a dictionary with the processed data to the locations list
-#         maison.append({"row": row, "images": images})
-#     # Configuration de la pagination avec le nombre total d'éléments et le nombre d'éléments par page
-#     pagination = Pagination(
-#         page=page, total=total_count, per_page=per_page, css_framework="bootstrap5"
-#     )
-
-#     # Fermeture du curseur et de la connexion
-#     cursor.close()
-#     conn.close()
-
-#     return render_template(
-#         "/page/achete_maison.html", maison_ifo=maison, pagination=pagination
-#     )
-
-
-
-
-import re
-
-def get_location_by_link(map_link):
-    match = re.search(r'@(-?\d+\.\d+),(-?\d+\.\d+)', map_link)
-    if match:
-        return "{}, {}".format(match.group(1), match.group(2))
-    return None
-
-GPS = get_location_by_link('https://maps.app.goo.gl/fyuCm1aefsdrWskNA')
-
-print(GPS)
+st.subheader('Résultat de la prévision')
+st.write(prevision)
